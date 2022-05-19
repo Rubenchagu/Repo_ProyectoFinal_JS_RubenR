d59c3028a15859f65c31fe26efdd6cd75ee37dc4
Ruben Ruiz <rubenchor@gmail.com>
2022-05-19 00:39:43 -0500

    Proyecto Final V2

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 
 //Declaración de variables
-let Monedaingresada
+let MonedaIngresada
 let CantidadIngresada
 let CantidadIngresadaUSD
 let MesesIngresados
 let TotalUSD
-let TotalMoneda
 let validar
 let ahorro
-let servicio
+let formConversor
+let resultadoConversor
+let simuladorAhorro
+let formSimulador
+let botonBusqueda
 
 //Declaración de objetos y Arrays
 class Moneda {
@@ -20,40 +23,28 @@ class Moneda {
     }      
 }
 
-const moneda1= new Moneda("Bitcoin", "BTC", 38000, 0.05)
-const moneda2= new Moneda("Etherium", "ETH", 2800, 0.04)
-const moneda3= new Moneda("Oasis Protocol", "ROSE", 0.17, 0.03)
-const moneda4= new Moneda("Cardano", "ADA", 0.81, 0.02)
-const moneda5= new Moneda("Gala Games", "GALA", 0.14, 0.01)
+const moneda1= new Moneda("bitcoin", "btc", 38000, 0.05)
+const moneda2= new Moneda("etherium", "eth", 2800, 0.04)
+const moneda3= new Moneda("oasis Protocol", "rose", 0.17, 0.03)
+const moneda4= new Moneda("cardano", "ada", 0.81, 0.02)
+const moneda5= new Moneda("gala games", "gala", 0.14, 0.01)
 
 const monedas = [moneda1, moneda2, moneda3, moneda4, moneda5]
 
+class Busqueda {
+    constructor(nombre, sigla, meses, ahorro) {
+        this.nombre=nombre
+        this.sigla=sigla
+        this.meses=meses
+        this.ahorro= ahorro
+    }      
+}
+
+const busquedas = []  //Guarda cada objeto de una nueva busqueda
+let Variasbusquedas = [] // Esto es un array de las busquedas
+let historial = []   //Aqui se guarda el historial en localstorage
+
 //Funciones
-const Convertidor = (MonedaIn, Cantidad) => {    
-    
-    const MonedaConvertir = monedas.find(moneda => moneda.nombre === MonedaIn || moneda.sigla === MonedaIn)
-    TotalUSD= Cantidad*MonedaConvertir.precioUSD
-    console.log(MonedaConvertir)
-    return TotalUSD
-}
-
-calcularAhorro = (MonedaIn, USD, meses) => {
-
-    const monedaAhorro = monedas.find(moneda => moneda.nombre === MonedaIn || moneda.sigla === MonedaIn)
-    TotalMoneda= 0
-    
-    for(i=1; i<=meses; i++) {
-
-        TotalMoneda= TotalMoneda + USD / monedaAhorro.precioUSD
-        ahorro= monedaAhorro.precioUSD * TotalMoneda
-        console.log(i + " Mes: ahorro en" + monedaAhorro.nombre + "= " + TotalMoneda)
-        console.log(i + " Mes: Precio estimado de " + monedaAhorro.nombre + "= " + monedaAhorro.precioUSD + " USD")
-        console.log(i + " Mes: Ahorro estimado= " + ahorro + " USD")
-        monedaAhorro.precioUSD = monedaAhorro.precioUSD + (monedaAhorro.precioUSD * monedaAhorro.porcentajeCrecimiento) 
-    }
-    return ahorro 
-}
-
 const validador = (MonedaIn, Cantidad, meses) => {
     
     if((monedas.some(moneda => moneda.nombre === MonedaIn || moneda.sigla === MonedaIn)==false) || (isNaN(Cantidad)) || (isNaN(meses))){
@@ -63,43 +54,111 @@ const validador = (MonedaIn, Cantidad, meses) => {
         if(isNaN(Cantidad))
             alert("Ingrese números válidos")
         if(isNaN(meses))
-        alert("Ingrese números válidos")
+            alert("Ingrese números válidos")
         validar=false    
     }else{validar=true}    
 }
 
-//Cuerpo
-do{
-    servicio = prompt("¿Qué desea hacer? - 1.Conversión || 2.Proyección ahorro || 3.Salir")
+const Convertidor = (MonedaIn, Cantidad) => {    
+    
+    const MonedaConvertir = monedas.find(moneda => moneda.nombre === MonedaIn || moneda.sigla === MonedaIn)
+    TotalUSD= Cantidad*MonedaConvertir.precioUSD
+    resultadoConversor.innerHTML = `
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <p class="card-text">${Cantidad} ${MonedaIn}= ${TotalUSD} USD</p>
+                <p class="card-text">TRM utilizada: 1 ${MonedaIn}= ${MonedaConvertir.precioUSD} </p>
+            </div>
+        </div>
+    `
+}
 
-    if(servicio == 1){
+const calcularAhorro = (USD, meses) => {
+    
+    busquedas.length=0
+    monedas.forEach((moneda) => {
+        let TotalMoneda= 0
         
-        do{
-            Monedaingresada= prompt("Ingrese la moneda a convertir a USD")
-            CantidadIngresada= prompt("Ingrese la cantidad a convertir")
-            validador(Monedaingresada, CantidadIngresada, 0)
-            
-        }while(validar==false)
+        for(i=1; i<=meses; i++) {
 
-        Convertidor(Monedaingresada, CantidadIngresada)
-        console.log("Total= " + TotalUSD + "  USD")
-        alert("Total= " + TotalUSD + "  USD")       
+            TotalMoneda= TotalMoneda + USD / moneda.precioUSD
+            ahorro= parseFloat(moneda.precioUSD * TotalMoneda)
+            moneda.precioUSD = moneda.precioUSD + (moneda.precioUSD * moneda.porcentajeCrecimiento) 
+        }
+    
+        busquedas.push(new Busqueda (moneda.nombre, moneda.sigla, meses, ahorro))
+    
+        simuladorAhorro.innerHTML +=`
+            <div class="card" style="width: 24rem;">
+                <div class="card-body">
+                    <h5 class="card-title">${(moneda.nombre.toUpperCase())}(${moneda.sigla})</h5>
+                    <p class="card-text">Total ahorrado en ${meses} meses=${ahorro.toFixed(2)}  USD</p>
+                </div>
+            </div>
+        `     
+    });
+
+    Variasbusquedas.push(busquedas)
+    
+
+}
+
+const verHistorial = () => {
+
+    Variasbusquedas.forEach(busqueda => {
+        historial.push(busqueda)
+        localStorage.setItem("historial", JSON.stringify(historial))
+    });
+    localStorage.setItem("historial", JSON.stringify(historial))
+
+    let VerBusquedas = JSON.parse(localStorage.getItem("historial"))
+    console.log(VerBusquedas)
+}
+
+//Main  
+
+if(localStorage.getItem("historial")){
+    historial = JSON.parse(localStorage.getItem("historial"))
+}else{
+    localStorage.setItem("historial", JSON.stringify(historial))
+}
+
+formConversor = document.querySelector("#formConversor")
+resultadoConversor = document.querySelector("#resultadoConversor")
+
+formConversor.addEventListener("submit", (evento) => {
+    
+    evento.preventDefault()
+    MonedaIngresada= (document.querySelector("#MonedaIngresada").value).toLowerCase()
+    CantidadIngresada= parseFloat(document.querySelector("#CantidadIngresada").value)
+    validador(MonedaIngresada, CantidadIngresada, 0)
+
+    if(validar==true){
+        Convertidor(MonedaIngresada, CantidadIngresada)
     }
-    else if(servicio==2){
+    formConversor.reset()
+})
 
-        do{
-            Monedaingresada= prompt("Ingrese la moneda en la que quiere ahorrar")
-            CantidadIngresadaUSD= prompt("Ingrese la cantidad en dólares mensual que quiere ahorrar")
-            MesesIngresados= parseInt(prompt("Ingrese la cantidad de meses que hará el ahorro"))
-            validador(Monedaingresada, CantidadIngresadaUSD, MesesIngresados)
-        }while(validar == false)
+formSimulador = document.querySelector("#formSimulador")
+simuladorAhorro = document.querySelector("#simuladorAhorro")
 
-        calcularAhorro(Monedaingresada, CantidadIngresadaUSD, MesesIngresados)
-        console.log("Total ahorro estimado en " + MesesIngresados + " meses = " + ahorro + " USD")
+formSimulador.addEventListener("submit", (evento) => {
 
-    }else if(servicio==3){
-        break
-    }
-    else{alert("opción invalida, vuelva a intentarlo")}
+    evento.preventDefault()
+    CantidadIngresadaUSD= parseFloat(document.querySelector("#CantidadIngresadaUSD").value)
+    MesesIngresados= parseFloat(document.querySelector("#MesesIngresados").value)
+    validador("btc", CantidadIngresadaUSD, MesesIngresados)
 
-}while(servicio!=1 || servicio!=2)
+    if(validar==true){
+        calcularAhorro(CantidadIngresadaUSD, MesesIngresados)
+    }    
+    formSimulador.reset()
+})
+
+botonBusqueda= document.querySelector("#botonBusqueda")
+
+botonBusqueda.addEventListener("click", () => {
+    
+    verHistorial()
+
+})
